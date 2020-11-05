9a57c62c6ef64cc9fa0ff57fecac0a8a3fb50b3e
Rishav Bhowmik <tgs.rishav.bhowmik@gmail.com>
2020-11-05 20:11:33 +0530

    FileStore integration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
+class FileStore{
+    static choose_file_n_upload(peer_ids = []){
+        return new Promise((resolve, reject)=>{
+            try{
+                var oReq = new XMLHttpRequest();
+                oReq.open("POST", "http://127.0.0.1:1445/set_file/owned_file", true);
+                const file_input = document.createElement('input')
+                file_input.type = 'file'
+                file_input.click()
+                file_input.addEventListener('change', function(){
+                    const this_file = this.files
+                    if(this_file.length < 1) {
+                        reject(Object.assign(Error("No File Selected"), {code:3}))
+                        return
+                    }
+
+                    oReq.onloadend = (ev)=>{
+                        console.log(ev);
+                        resolve(JSON.parse(ev.target.response))
+                    }
+
+                    oReq.ontimeout = reject
+
+                    const reader = new FileReader();
+                    reader.onload = function(evt) {
+                        console.log(evt.target);
+                        console.log(new Blob([evt.target.result],
+                            {
+                                type: this_file[0].type,
+                                name: this_file[0].name
+                            }
+                        ));
+                      //resolve() 
+                        const form = new FormData()
+                        form.append('user_token', FileStore.auth_cache.user_token || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlveW8iLCJ1c2VySWQiOiI1MDdmMTkxZTgxMGMxOTcyOWRlODYwZWEiLCJpYXQiOjE2MDMzNjMzMDh9.8vK77Keb0mw8JbztljIEMb384wu7BL-T3wwU1dhggN4")
+                        form.append('allow_users', peer_ids.join())
+                        form.append('file', new Blob([evt.target.result],
+                            {
+                                type: this_file[0].type,
+                                name: this_file[0].name
+                            }
+                        ), this_file[0].name)
+                        oReq.send(form)
+                    };
+                    reader.readAsArrayBuffer(this.files[0]);
+                })
+                
+            }catch(e){
+                reject(e)
+            }
+        })
+    }
+    static blob_upload(blob, fname = Date().toString(), user_token){
+        return new Promise((resolve, reject)=>{
+            try{
+                if(!(blob instanceof Blob)) throw Error("blob should be instance of Blob")
+                if(typeof fname !== 'string') throw Error("fname should be type String")
+                var oReq = new XMLHttpRequest();
+                oReq.open("POST", "http://127.0.0.1:1445/set_file/owned_file", true);
+                oReq.onloadend = (ev)=>{
+                    console.log(ev);
+                    resolve(JSON.parse(ev.target.response))
+                }
+                oReq.ontimeout = reject
+
+                const form = new FormData()
+                //remove default owner when jwt is done
+                form.append('user_token', user_token || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InlveW8iLCJ1c2VySWQiOiI1MDdmMTkxZTgxMGMxOTcyOWRlODYwZWEiLCJpYXQiOjE2MDMzNjMzMDh9.8vK77Keb0mw8JbztljIEMb384wu7BL-T3wwU1dhggN4")
+                form.append('file', new Blob([evt.target.result],
+                    {
+                        type: this_file[0].type,
+                        name: this_file[0].name
+                    }
+                ), this_file[0].name)
+                oReq.send(form)
+
+            }catch(e){
+                reject(e)
+            }
+        })
+    }
+}
+FileStore.auth_cache = {user_token:null}
+
 class ChatLines{
     constructor(reciver_id /* mongodb_objectID */){
         this.reciver_id = reciver_id
@@ -7,6 +91,26 @@ class ChatLines{
         const header = {type:'text'}
         const body = text.toString()
         this.lines.push({header, body})
+    }
+    async add_line_file_auto_user_input_sync(){
+        const this_class = this
+        try{
+            const file = await FileStore.choose_file_n_upload([this_class.reciver_id])
+            
+            const header = {
+                "type": "file",
+                "file_token": file.file_token,
+                "preview_mime_type": file.preview? file.preview.type : false,
+                "meta" : file.about_file
+            }
+            const body = file.preview ? file.preview.dataurl : ""
+
+            this_class.lines.push({header, body})
+            
+        }catch(e){
+            console.log(e)
+            return null
+        }
     }
     frame_request(req_id){
         return Object.assign(this, {req_id})
@@ -148,4 +252,4 @@ class ChatSystem{
     }
 }
 
-export {ChatLines, ChatSystem}
+export {ChatLines, ChatSystem, FileStore}
